47b97cf23ca0b2c663aebb51b88e8b7c31a7ae65
Gavin White <horace5413@gmail.com>
2020-01-19 12:43:49 -0800

    ScreenObject base saving and loading tested, works.

--- Ironbound/ScreenObject.cs
+++ Ironbound/ScreenObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,11 @@ namespace Ironbound {
         public static int MaxOnscreen = 1024;
         public static List<ScreenObject> ScreenObjects => screenObjects;
         static List<ScreenObject> screenObjects = new List<ScreenObject>();
+
+        static ScreenObjectManager() {
+            if (!Directory.Exists(SavePath))
+                Directory.CreateDirectory(SavePath);
+        }
 
         static void TryAddScreenObject(ScreenObject screenObject) {
             if (screenObjects.Count >= MaxOnscreen) { // Is there room?
@@ -32,8 +38,32 @@ namespace Ironbound {
             screenObjects.Remove(screenObject);
         }
 
-        public static void Save(ScreenObject screenObject) {
+        public static bool IsSaved(string name) {
+            return File.Exists($"{SavePath}\\{name}.ieo");
+        }
 
+        public static string[] GetSavedObjects() {
+            string[] paths = Directory.GetFiles(SavePath);
+            string[] names = new string[paths.Length];
+
+            for (int i = 0; i < paths.Length; i++)
+                names[i] = paths[i].Trim(SavePath.ToCharArray());
+
+            return names;
+        }
+
+        static string GetSavePathFromName(string name) => $"{SavePath}\\{name}.ieo";
+        public static void Save(ScreenObject screenObject) {
+            File.WriteAllText(GetSavePathFromName(screenObject.name), screenObject.Save());
+        }
+        public static ScreenObject Load(string name) {
+            ScreenObject load = new ScreenObject();
+            load.LoadFromString(File.ReadAllText(GetSavePathFromName(name)));
+
+            return load;
+        }
+        public static void LoadAndAdd(string name) {
+            TryAddScreenObject(Load(name));
         }
     }
 
@@ -41,21 +71,54 @@ namespace Ironbound {
         public string name;
         public int x, y, width, height;
         public double lastViewedTime;
+        protected int saveLength = 4;
 
         public virtual string Save() {
-            return $"{name}|{x},{y}|{width}|{height}|{lastViewedTime}";
+            return $"{name}|{x},{y}|{width},{height}|{lastViewedTime}";
         }
 
         public virtual void LoadFromString(string save) {
-            string[] data = save.Split("so(|)".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            try {
+                string[] data = save.Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                if (data.Length != saveLength)
+                    DebugManager.LogError("Failed to load entity: Actual length did not match expected length.");
 
-            // name
-            name = data[0];
+                // name
+                name = data[0];
 
-            // x, y
-            string[] pos = data[1].Split(',');
-            //if (int.TryParse(pos[0], out int nx))
+                // x, y
+                string[] pos = data[1].Split(',');
+                if (int.TryParse(pos[0], out int nx) && int.TryParse(pos[1], out int ny)) {
+                    x = nx;
+                    y = ny;
+                }
+                else {
+                    DebugManager.LogError($"Failed to load entity {name} at position ({pos[0]},{pos[1]}).");
+                }
 
+                // width, height
+                string[] dims = data[2].Split(',');
+                if (int.TryParse(dims[0], out int nw) && int.TryParse(dims[1], out int nh)) {
+                    width = nw;
+                    height = nh;
+                }
+                else {
+                    DebugManager.LogError($"Failed to load entity {name} at dimensions ({dims[0]},{dims[1]}).");
+                }
+
+                // last viewed time
+                if (double.TryParse(data[3], out double lvt)) {
+                    lastViewedTime = lvt;
+                }
+                else {
+                    DebugManager.LogError($"Failed to load entity {name} at lastViewedTime ({data[3]}).");
+                }
+            }
+            catch (Exception e) {
+                DebugManager.LogError($"Generic failiure loading entity: {e}");
+            }
         }
+
+        public override string ToString() => Save();
     }
 }
