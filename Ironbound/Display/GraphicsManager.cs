20a56a6ffce40390fa350de73da355fa591d21ed
Gavin White <horace5413@gmail.com>
2020-01-20 09:51:25 -0800

    Add graphics manager, main game class.

---
+++ Ironbound/Display/GraphicsManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PixelEngine;
+
+namespace Ironbound.Display {
+    public static class GraphicsManager {
+        public static Ironbound Game { get; private set; }
+
+        public static void RegisterGame(Ironbound game) => Game = game;
+    }
+}
