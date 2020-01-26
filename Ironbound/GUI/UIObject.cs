72a919b9c27fe9556adf72996c93bdef6e10c54d
Gavin White <horace5413@gmail.com>
2020-01-26 11:23:20 -0800

    UIObject is IDrawable

--- Ironbound/GUI/UIObject.cs
+++ Ironbound/GUI/UIObject.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ironbound.Display;
 
 namespace Ironbound.GUI {
     public static class UIManager {
@@ -13,7 +14,7 @@ namespace Ironbound.GUI {
 
     }
 
-    public class UIObject : IUpdateable {
+    public class UIObject : IUpdateable, IDrawable {
         public string name;
         public int x, y, width, height;
         Rectangle Bounds => new Rectangle(x, y, width, height);
@@ -22,5 +23,6 @@ namespace Ironbound.GUI {
         public virtual bool Intersects(int x, int y) => Bounds.Intersects(new Point(x, y));
 
         public virtual void Update(float elapsed) { }
+        public virtual void Draw(PixelEngine.Game game) { }
     }
 }
